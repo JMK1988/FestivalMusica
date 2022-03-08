a6baa889efbb4a67bef3704aeea76e665dd386cc
JMK1988 <juanmaster23@gmail.com>
2022-03-08 16:32:47 -0300

    8/3

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,38 @@
-const { src, dest, watch } = require('gulp');
-
+const { src, dest, watch, parallel } = require('gulp');
+//css
 const sass = require('gulp-sass')(require('sass'));
-
 const plumber = require('gulp-plumber');
+//imagenes 
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp'); 
+
+
 
 function css( done ){
     src('src/scss/**/*.scss')//identificar el archivo .scss a copilar
         .pipe( plumber())
         .pipe( sass() )//copilarlo
         .pipe( dest('build/css') )//almacenarlo
+    done();
+}
+
+function imagenes(done){
+    src('src/img/**/*.{png,jpg}')
+    .pipe( )
+
+    done();
+}
+
+function versionWebp(done){
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+
+        .pipe( webp(opciones) )
+        .pipe (dest('build/img') ) 
+
     done();
 }
 
@@ -20,4 +44,5 @@ function dev( done ){
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp
+exports.dev = parallel( versionWebp, dev);
