9addfb93f25fab3fb4d1ddc44bc2c6199f5f23a3
juan marcos kruppa <juanmaster23@gmail.com>
2022-05-19 18:30:13 -0300

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,26 @@ const { src, dest, watch, parallel } = require('gulp');
 //css
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps')
 //imagenes 
 const cache = require('gulp-cache')
 const imagemin = require('gulp-imagemin')
-const webp = require('gulp-webp'); 
+const webp = require('gulp-webp');
 const avif = require('gulp-avif');
-
+//javascript
+const terser = require('gulp-terser-js');
 
 
 function css( done ){
     src('src/scss/**/*.scss')//identificar el archivo .scss a copilar
+        .pipe( sourcemaps.init())
         .pipe( plumber())
         .pipe( sass() )//copilarlo
+        .pipe( postcss([autoprefixer(), cssnano()]))
+        .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css') )//almacenarlo
     done();
 }
@@ -36,7 +44,7 @@ function versionWebp(done){
     src('src/img/**/*.{png,jpg}')
 
         .pipe( webp(opciones) )
-        .pipe (dest('build/img') ) 
+        .pipe (dest('build/img') )
 
     done();
 }
@@ -54,6 +62,9 @@ function versionAvif(done){
 }
 function javascript(done){
     src('src/js**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser())
+        .pipe(sourcemaps.write())
         .pipe(dest('build/js'));
     done()
 }
@@ -62,10 +73,8 @@ function dev( done ){
 
     watch('src/scss/**/*.scss', css)
     watch('src/js/**/*.js', javascript)
-
     done();
 }
-
 exports.css = css;
 exports.js = javascript;
 exports.imagenes = imagenes;
